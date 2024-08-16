505f44ededff242be5f7cfbb3b42389fa4142725
luiz-decio <luizgdecio@gmail.com>
2024-08-16 15:58:19 -0300

    build: added requirements and dockerfile

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def main():
     # Selection of conversion type
     option = st.selectbox("Choose conversion", ["Parquet to CSV", "CSV to Parquet"])
     
-    loaded_file = st.file_uploader(f"Load your file {option.split()[0]}", type=[option.split()[0].lower()])
+    loaded_file = st.file_uploader(f"Load your {option.split()[0]} file", type=[option.split()[0].lower()])
 
     if option == "CSV to Parquet":
         option_2 = st.selectbox("Choose delimiter", [";", ","])
@@ -20,11 +20,11 @@ def main():
                 df = pd.read_parquet(io.BytesIO(loaded_file.getvalue()))
 
                 # Show a basic preview of the file
-                st.write("DataFrame Preview")
+                st.write("Preview")
                 st.dataframe(df.head())  # Show the first 5 rows
 
                 # Convert to CSV
-                csv = df.to_csv(index=False).encode("utf-8")
+                csv = df.to_csv(index=False).encode("latin-1")
                 st.download_button(
                     label="Download CSV",
                     data=csv,
@@ -35,10 +35,10 @@ def main():
             elif option == "CSV to Parquet":
                 # Read the CSV file into a pandas Dataframe
 
-                df = pd.read_csv(loaded_file, sep=option_2)
+                df = pd.read_csv(loaded_file, sep=option_2, encoding="latin-1")
 
                 # Show a basic preview of the file
-                st.write("DataFrame Preview")
+                st.write("Preview")
                 st.dataframe(df.head())  # Show the first 5 rows
 
                 # Convert to Parquet
@@ -54,7 +54,7 @@ def main():
         except Exception as e:
             st.error(f"An error occurred while trying to convert the file: {e}")
     else:
-        st.warning(f"Please, load a file {option.split()[0]}.")
+        st.warning(f"Please, load a {option.split()[0]} file.")
 
 if __name__ == "__main__":
     main()
