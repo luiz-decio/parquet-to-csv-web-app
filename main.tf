2b6503f45f2342aa1ba206b8dc58d5d78c5bacc0
luiz-decio <luizgdecio@gmail.com>
2024-08-29 19:03:16 -0300

    docs: update terraform main

--- main.tf
+++
@@ -1,9 +0,0 @@
-variable "ami" {}
-
-provider "aws" {
-    region = "us-east-1"
-}
-
-resource "aws_instance" "app_instance" {
-  ami = var.ami
-}
---
+++ terraform/main.tf
@@ -0,0 +1,20 @@
+variable "ami" {}
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "app_instance" {
+  ami = var.ami
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
+
+  user_data = <<-EOF
+            !#/bin/bash
+            sudo apt-get update
+            sudo apt-get install -y docker.io git
+            sudo systemctl start docker
+            sudo systemctl enable socker
+  
+}
